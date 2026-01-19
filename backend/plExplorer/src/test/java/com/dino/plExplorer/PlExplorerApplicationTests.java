2769d99c2ad3243281ae963b6564988c744674e2
Dino-HodzicTumpic <dino.hodzic-tumpic@fer.unizg.hr>
2026-01-19 14:57:49 +0100

    Initial commit

---
+++ backend/plExplorer/src/test/java/com/dino/plExplorer/PlExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dino.plExplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlExplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
